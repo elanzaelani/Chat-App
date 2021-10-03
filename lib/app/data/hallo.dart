353540bc49fa2f1ced93b7b10f53d5370ef802b6
elzaelani <ezaelani72@gmail.com>
2021-10-03 21:47:09 +0700

    tambah file baru

---
+++ lib/app/data/hallo.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class hallo extends StatelessWidget {
+  const hallo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
